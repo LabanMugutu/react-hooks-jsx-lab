c6b28f6a89b55d98dcc74ac479da77c1d62800c6
LabanMugutu <labanmugutu@gmail.com>
2025-08-21 14:09:20 +0300

    finalizes repo

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,11 @@
 import React from "react";
-import { image } from "../data/data";
 
-function About() {
-  return <div>About</div>;
+export default function About() {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is some text about me.</p>
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+    </div>
+  );
 }
-
-export default About;
